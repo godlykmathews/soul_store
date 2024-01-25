30bde136d360cf34f94f62c2c04affdd912889bc
godlykmathews <godlykmathews@outlook.com>
2024-01-25 21:19:28 +0530

    Product

---
+++ helpers/product_helper.js
@@ -0,0 +1,12 @@
+let products = [
+    
+    {
+        name: "CONVERSE",
+        price: 899,
+        description: "Top End Quality. 100% Same As OG.",
+        shipping: "Free",
+        image : "assets/products/shoes/converse (1).jpg" 
+    },
+
+]
+module.exports = products;
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,14 @@
 var express = require("express");
 var router = express.Router();
-var images = require("../helpers/gallery_helper");
+var images = require("../helpers/product_helper");
+const products = require("../helpers/product_helper");
 
 router.get("/", function (req, res, next) {
   res.render("user/home", {title: "Home", home_active:true})
 });
 
 router.get("/men", function (req, res, next) {
-  res.render("user/men", {title: "Men's", men_active:true})
+  res.render("user/men", {title: "Men's", men_active:true}, products)
 });
 
 router.get("/women", function (req, res, next) {
@@ -20,6 +21,14 @@ router.get("/kids", function (req, res, next) {
 
 router.get("/about", function (req, res, next) {
   res.render("user/about", {title: "About"})
+});
+
+router.get("/product-detail", function (req, res, next) {
+  res.render("user/single-product", {title: "Product"})
+});
+
+router.get("/contact", function (req, res, next) {
+  res.render("user/contact", {title: "Contact"})
 });
 
 router.post("/", (req, res) => {
