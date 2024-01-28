308b913ef09201f70c6d00305ff0d39203733b1e
godlykmathews <godlykmathews@outlook.com>
2024-01-28 13:40:17 +0530

    shoe

--- helpers/product_helper.js
+++ helpers/product_helper.js
@@ -1,12 +1,11 @@
 let products = [
     
     {
-        name: "CONVERSE",
+        name: "CONVERS",
         price: 899,
         description: "Top End Quality. 100% Same As OG.",
         shipping: "Free",
-        image : "assets/products/shoes/converse (1).jpg" 
+        image : "assets/products/shoes/converse.jpg" 
     },
-
 ]
 module.exports = products;
--- routes/index.js
+++ routes/index.js
@@ -1,38 +1,37 @@
 var express = require("express");
 var router = express.Router();
-var images = require("../helpers/product_helper");
-const products = require("../helpers/product_helper");
+const products = require("../helpers/product_helper").default;
 
 router.get("/", function (req, res, next) {
-  res.render("user/home", {title: "Home", home_active:true})
+  res.render("user/home", { title: "Home", home_active: true });
 });
 
 router.get("/men", function (req, res, next) {
-  res.render("user/men", {title: "Men's", men_active:true}, products)
+  res.render("user/men", { title: "Men's", men_active: true, products: products });
 });
 
 router.get("/women", function (req, res, next) {
-  res.render("user/women",{title: "Women's", women_active:true})
+  res.render("user/women", { title: "Women's", women_active: true });
 });
 
 router.get("/kids", function (req, res, next) {
-  res.render("user/kids", {title: "kid's", kids_active:true})
+  res.render("user/kids", { title: "kid's", kids_active: true });
 });
 
 router.get("/about", function (req, res, next) {
-  res.render("user/about", {title: "About"})
+  res.render("user/about", { title: "About" });
 });
 
 router.get("/product-detail", function (req, res, next) {
-  res.render("user/single-product", {title: "Product"})
+  res.render("user/single-product", { title: "Product" });
 });
 
 router.get("/contact", function (req, res, next) {
-  res.render("user/contact", {title: "Contact"})
+  res.render("user/contact", { title: "Contact" });
 });
 
 router.post("/", (req, res) => {
-
+  
 });
 
 module.exports = router;
